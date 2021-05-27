7d4fcef514bf1499872ee2b9fe780196fe452832
takanokana <iricocco3@gmail.com>
2021-05-27 15:04:05 +0900

    プライベートネット

--- iam_role/main.tf
+++ iam_role/main.tf
@@ -1,33 +1,33 @@
-variable "name" {}
-variable "policy" {}
-varialbe "identifier" {}
+# variable "name" {}
+# variable "policy" {}
+# variable "identifier" {}
 
-resource "aws_iam_role" "default" {
-    name = var.name
-    assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+# resource "aws_iam_role" "default" {
+#     name = var.name
+#     assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
 
-data "aws_iam_policy_document" "assume_role" {
-    statement {
-        actions = ["sts:AssumeRole"]
-    }
+# data "aws_iam_policy_document" "assume_role" {
+#     statement {
+#         actions = ["sts:AssumeRole"]
+#     }
 
-    principals {
-        type = "Service"
-        identifiers = [var.identifier]
-    }
-}
+#     principals {
+#         type = "Service"
+#         identifiers = [var.identifier]
+#     }
+# }
 
-resource "aws_iam_policy" "default" {
-    name = var.name
-    policy = var.policy
-}
+# resource "aws_iam_policy" "default" {
+#     name = var.name
+#     policy = var.policy
+# }
 
-resource "aws_iam_policy_attachment" "default" {
-    role = aws_iam_role.default.name
-    policy_arn = aws_iam_policy.default.arn
-}
+# resource "aws_iam_policy_attachment" "default" {
+#     role = aws_iam_role.default.name
+#     policy_arn = aws_iam_policy.default.arn
+# }
 
-output "iam_role_name" {
-    value = aws_iam_role.default.name
-}
+# output "iam_role_name" {
+#     value = aws_iam_role.default.name
+# }
